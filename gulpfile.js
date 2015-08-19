5420fe72c8d1e760e7196f172c8138b1df5f9c50
vincent.sue <gnux123@gmail.com>
2015-08-19 14:16:56 +0800

    add gulpfile.js and package.json use auto run process.

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp')
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+var vers = "1.0.0.0.0";
+
+gulp.task('sass', ['clean'], function(){
+    gulp.src('./scss/{*/,**/}*.scss')
+        .pipe(sass({
+              includePaths: ['./scss/includes/'],
+              outputStyle: 'nested'
+          }).on('error', sass.logError)
+        )
+        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+        .pipe(gulp.dest('./.csscache'))
+        .pipe(rename({ suffix: "-" + vers }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/{*/,**/}*.scss', ['sass']);
+});
+
+//clean temp
+gulp.task('clean', function(cb){
+    del(['.csscache'], {read: false}, cb);
+});
+
+
+gulp.task('watch', function(){
+    gulp.start('sass:watch');
+});
+
+gulp.task('build', function() {
+     gulp.start('sass');
+});
