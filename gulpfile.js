2ab172fa759d04069506cb6e00651ef4aa6dd613
vincent.sue <gnux123@gmail.com>
2015-08-23 13:10:08 +0800

    fixed gulfile.js tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     del = require('del'),
+    copy = require('gulp-copy'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
@@ -13,8 +14,8 @@ var _address = {
   Address: '.', //'D:/projects/NETWWebsite2.0/01_Branch/Branch_Task20150820-CSS/TWNewEgg.ECWeb/TWNewEgg.ECWeb/Themes'
   sass: 'scss/{*/,**/}*.scss',
   css: 'css',
-  cache: 'css/_csstmp',
-  styleguide: 'styleguide/styles',
+  cache: 'css/_csstmp/',
+  styleguide: 'styleguide/styles/',
   include: 'scss/includes/'
 }
 
@@ -28,41 +29,36 @@ gulp.task('sass', function(){
       )
       //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
       .pipe(gulp.dest(_address.cache))
+      .pipe(gulp.dest(_address.styleguide))
       .pipe(styleguide({
         out: 'styleguide',
         name: 'Newegg-CSS documents v'+ vers,
-        include: [_address.cache+'/common.css',_address.cache+'/RWD.css'],
+        include: [_address.styleguide+'/common.css',_address.styleguide+'/RWD.css'],
         'no-minify': false
       }));
+
 });
 
 //cssmin task
 gulp.task('cssmin', function(){
-  gulp.src(_address.cache+'/*.css')
+  gulp.src(_address.cache+'*.css')
       .pipe(minifyCss({compatibility: 'ie8'}))
       .pipe(rename({ suffix: "-" + vers }))
       .pipe(gulp.dest(_address.css));
 });
 
-//sass watch livetype
-gulp.task('sass:watch', function () {
-  gulp.watch(_address.sass, ['build']);
-});
-
-//concatcss
-// gulp.task('concat-common-css', function(){
-//     gulp.src(paths.cache+'/common.css')
-//         .pipe(gulp.dest(paths.styleguide))
-//         .pipe(minifyCss({compatibility: 'ie8'});
-// });
-
 //clean temp
 gulp.task('clean', function(cb){
-    del(['css'],cb);
+    del(['css','styleguide'],cb);
+});
+
+//sass watch livetype
+gulp.task('sass:watch', function () {
+  gulp.watch(_address.sass, ['sass','cssmin']);
 });
 
 //build task
 gulp.task('build',['sass','cssmin']);
 
 //watch task
-gulp.task('watch', ['clean','sass:watch']);
+gulp.task('server', ['clean','sass:watch']);
