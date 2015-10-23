24dca0b8ecd6a504ad564cb8478be293f1a90cc4
vincent.sue <gnux123@gmail.com>
2015-10-23 17:32:33 +0800

    調整主選單樣式及分類頁面layout設定.

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task('sass', function(){
             .pipe(plumber())
             .pipe(sass({
                     includePaths: [_address.include],
-                    outputStyle: 'expanded',
+                    outputStyle: 'compressed',
                     precision: 10,
                     errLogToConsole: true
                 }).on('error', sass.logError)
@@ -51,7 +51,6 @@ gulp.task('sass', function(){
             //.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
             .pipe(gulp.dest(_address.cache))
             .pipe(replace('img/', '/Themes/img/')) //replace imgPath to stable server
-            .pipe(minifyCss({compatibility: 'ie8'}))
             .pipe(rename({ suffix: "-" + vers }))
             .pipe(plumber.stop())
             .pipe(gulp.dest(_address.css));
